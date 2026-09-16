433e9795f205d0d8bfe173912d4b8503d5c2a7ca
Jon Poulton <jpoulton@pm.me>
2026-09-16 11:52:58 +0100

    d

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -12,9 +12,19 @@ pluginManagement {
 }
 
 plugins {
+  id("com.gradle.develocity") version "4.5.1"
+  id("io.github.gmazzo.publications.report") version "1.4.1"
   id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
 }
 
 enableFeaturePreview("STABLE_CONFIGURATION_CACHE")
+
+develocity.buildScan {
+  if (!gradle.startParameter.isBuildScan) {
+    publishing.onlyIf { it.isAuthenticated }
+  }
+
+  uploadInBackground = false
+}
 
 include(":compiler", ":compiler-tests", ":gradle-plugin", ":runtime")
